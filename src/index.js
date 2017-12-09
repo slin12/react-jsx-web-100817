6f99cfdbf01bb2d7e0a48e0d7cd24e9e57e27b18
Shirley <s.lin12@gmail.com>
2017-12-09 16:45:38 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 // Code goes here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { message } from './components/foo.js'
+
+ReactDOM.render(
+  message,
+  document.getElementById('global')
+);
